54f51169dd6780524758e9691bec0700cdb4ddb6
Royce Santo <roycesanto@yahoo.com>
2017-04-23 07:34:57 +0800

    Fade in to Scene 1

---
+++ Assets/ScreenFadeout.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScreenFadeout : MonoBehaviour {
+
+    public Image ImgFade;
+    public float fadeOutTime;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fadeout());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    IEnumerator Fadeout()
+    {
+
+            ImgFade.canvasRenderer.SetAlpha(1f);
+            ImgFade.gameObject.SetActive(true);
+            ImgFade.CrossFadeAlpha(0f, fadeOutTime, false);
+            yield return new WaitForSeconds(fadeOutTime);
+    }
+}
