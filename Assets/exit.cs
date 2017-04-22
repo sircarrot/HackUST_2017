911f0db196bec110dc743d5eef0d8d8db7bb9ce8
Alvin Tjhie <alvinwiguna@yahoo.com>
2017-04-23 05:53:15 +0800

    Prefabs MASTER STAGE 1

---
+++ Assets/exit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public int dark_light; //dark = 0, light = 1
+    public int scene;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        PlayerPrefs.SetInt("Scene"+scene, dark_light);
+        PlayerPrefs.Save();
+        Application.LoadLevel(scene + 1);
+    }
+}
