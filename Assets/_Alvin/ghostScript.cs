1fb9566a6c0ebf08dd7af323bad3fb712054fdc6
Royce Santo <roycesanto@yahoo.com>
2017-04-22 20:39:14 +0800

    Walk animation and idle for all players and ghost finished

--- Assets/_Alvin/ghostScript.cs
+++ Assets/_Alvin/ghostScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ghostScript : MonoBehaviour {
 
     public GameObject Parent;
+
+    public Sprite idle;
 	// Use this for initialization
 	void Start ()
     {
@@ -14,6 +16,24 @@ public class ghostScript : MonoBehaviour {
 	void Update ()
     {
         Vector3 tmpPosition = new Vector3(-1 * Parent.transform.position.x, Parent.transform.position.y, this.transform.position.z);
+        Vector3 moveVec = Parent.GetComponent<PlayerMovementScript>().moveVec;
         this.transform.position = tmpPosition;
-	}
+
+        if (moveVec.x > 0.01)
+        {
+            gameObject.GetComponent<Animator>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (moveVec.x < -0.01)
+        {
+            gameObject.GetComponent<Animator>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().sprite = idle;
+        }
+
+    }
 }
