1ff9969954c0f8da2f94f11239416791378cacde
Alvin Tjhie <alvinwiguna@yahoo.com>
2017-04-23 03:32:54 +0800

    Prefab for days son meta data

---
+++ Assets/_Alvin/doorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorScript : MonoBehaviour {
+
+    public GameObject Door;
+    public bool on = true;
+    public float TimeReal = 1;
+    Vector3 tmpNow;
+    Vector3 tmpTarget;
+    // Use this for initialization
+    void Start ()
+    {
+        tmpNow = Door.transform.position;
+        tmpTarget = new Vector3(Door.transform.position.x, Door.transform.position.y - 27.4f, Door.transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(on == false)
+        {
+            if(TimeReal>0)
+            {
+                TimeReal -= Time.deltaTime * 1;
+            }
+            
+            Door.transform.position = Vector3.Lerp(tmpTarget,tmpNow,TimeReal);
+        }
+	}
+}
---
+++ Assets/switchScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchScript : MonoBehaviour
+{
+    public GameObject doorScript_Var;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        doorScript_Var.GetComponent<doorScript>().on = false;
+        this.transform.localScale = new Vector3(-1,1,1);
+    }
+}
