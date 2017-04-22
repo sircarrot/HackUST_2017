138a5025f3afe27f25562a8825419348ee4b8871
Alvin Tjhie <alvinwiguna@yahoo.com>
2017-04-22 16:29:47 +0800

    Second Commit, Game basic done Switch and excliusive passable block - WIP

---
+++ Assets/_Alvin/PlayerMovementScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public int Player;
+    public float moveSpeed = 0.5f;
+    public float JumpSpeed = 5f;
+
+    public bool IsJumping = false;
+
+    Rigidbody2D myBody;
+	// Use this for initialization
+	void Start ()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal_" + Player), 0, 0) * moveSpeed;
+        this.transform.Translate(moveVec);
+    }
+
+    public void JumpCommand()
+    {
+        if (IsJumping == false)
+        {
+            myBody.velocity += JumpSpeed * Vector2.up;
+            IsJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            IsJumping = false;
+        }
+    }
+}
