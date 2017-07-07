f0a9ac7c880f6825688dcaadb14c8ed1dee0ca00
Royce Santo <roycesanto@yahoo.com>
2017-07-07 18:01:46 +0800

    added music and click effect to the end credits

--- Assets/_Royce/Scripts/restart.cs
+++ Assets/_Royce/Scripts/restart.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class restart : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Image fadeBox;
+    public float fadetime;
+    public AudioClip clickSound;
+
+    // Use this for initialization
+    void Start () {
+        fadeBox.canvasRenderer.SetAlpha(0f);
+    }
+
+
 
     public void resetPlayerPref()
     {
+        gameObject.GetComponent<AudioSource>().PlayOneShot(clickSound);
+
         PlayerPrefs.SetInt("CurScene", 1);
         PlayerPrefs.SetInt("Choice1", 0);
         PlayerPrefs.SetInt("Choice2", 0);
@@ -19,6 +28,16 @@ public class restart : MonoBehaviour {
         PlayerPrefs.SetInt("Choice4", 0);
         PlayerPrefs.SetInt("Choice5", 0);
 
+        StartCoroutine(fadeToMain());
+    }
+
+    IEnumerator fadeToMain()
+    {
+        yield return new WaitForSeconds(1);
+        fadeBox.gameObject.SetActive(true);
+        fadeBox.CrossFadeAlpha(1f, fadetime, false);
+        yield return new WaitForSeconds(fadetime);
         SceneManager.LoadScene("MainMenu");
     }
+
 }
