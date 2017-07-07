9049f75ba575e857e3448fda74d155e3ee2a98d1
Royce Santo <roycesanto@yahoo.com>
2017-07-07 17:35:57 +0800

    Fixed all text colors, childstory line number fixed, credits restart finished.

---
+++ Assets/_Royce/Scripts/restart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void resetPlayerPref()
+    {
+        PlayerPrefs.SetInt("CurScene", 1);
+        PlayerPrefs.SetInt("Choice1", 0);
+        PlayerPrefs.SetInt("Choice2", 0);
+        PlayerPrefs.SetInt("Choice3", 0);
+        PlayerPrefs.SetInt("Choice4", 0);
+        PlayerPrefs.SetInt("Choice5", 0);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
